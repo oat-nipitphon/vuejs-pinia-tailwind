e5616e5db6a3012dbeeca475c7d493df50205726
HelloAmaZingWorld <hello.ama2ing.w0r1d237@gmail.com>
2025-01-04 00:00:43 +0700

    update app navbar

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -10,12 +10,13 @@ const authStore = reactive({
   user: {
     fullname: "John Doe",
     email: "john.doe@example.com",
-    profileImage: "https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/461897536_3707658799483986_794048670785055411_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHVG0UH5FgwbVkdtl70b39it0I862Qbciu3QjzrZBtyK4PmJExwkjQwGNMpc0Sbm9HeXRE2Yi7Fvc_GrvrUrXJN&_nc_ohc=wr6rT2BFWA0Q7kNvgFDoePu&_nc_oc=Adj1Q1vcvazXT03cBW3bKQy6YLG6vPsppIdOJbLZ_A9RZWcLTAHpZ8nDVf4qZwTenZRbtsMxJaUYa-kRuj4tRRVV&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=AY9Yz97Zkb24BwEjmpnTbcu&oh=00_AYAi7KVwtgN_U5v4vvoqNz0O8Ur9FXRd65TElAtDW0v0fw&oe=677D9B71",
+    profileImage: "https://example.com/default-profile.jpg",
   },
 });
 
-
 const isDropdownVisible = ref(false);
+
+// Methods
 const toggleDropdown = () => {
   isDropdownVisible.value = !isDropdownVisible.value;
 };
@@ -29,180 +30,199 @@ const logout = () => {
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-
-      <!-- Logo -->
-      <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/461897536_3707658799483986_794048670785055411_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHVG0UH5FgwbVkdtl70b39it0I862Qbciu3QjzrZBtyK4PmJExwkjQwGNMpc0Sbm9HeXRE2Yi7Fvc_GrvrUrXJN&_nc_ohc=wr6rT2BFWA0Q7kNvgFDoePu&_nc_oc=Adj1Q1vcvazXT03cBW3bKQy6YLG6vPsppIdOJbLZ_A9RZWcLTAHpZ8nDVf4qZwTenZRbtsMxJaUYa-kRuj4tRRVV&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=AY9Yz97Zkb24BwEjmpnTbcu&oh=00_AYAi7KVwtgN_U5v4vvoqNz0O8Ur9FXRd65TElAtDW0v0fw&oe=677D9B71" class="h-8" alt="Logo" />
-        <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">App Name</span>
-      </a>
-
-      <!-- Authenticated User Menu -->
-      <div v-if="authStore.isAuthenticated">
-
-        <div class="flex items-start space-x-3 rtl:space-x-reverse">
-           <!-- Button Open Main Menu Toggle -->
-           <button
-            data-collapse-toggle="navbar-user"
-            type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-user"
-            aria-expanded="false"
+  <header>
+    <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+      <div
+        class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
+      >
+        <a
+          href="https://flowbite.com"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="h-8"
+            alt="Flowbite Logo"
+          />
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >Flowbite</span
           >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-
-          <!-- Main Menu -->
-          <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-user"
+        </a>
+        <button
+          data-collapse-toggle="mega-menu-full"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mega-menu-full"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          id="mega-menu-full"
+          class="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1"
         >
           <ul
-            class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            class="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
           >
             <li>
-              <RouterLink
+              <a
+                href="#"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 aria-current="page"
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                :to="{ name: 'HomeView' }"
+                >Home</a
               >
-                Home
-              </RouterLink>
             </li>
             <li>
-              <RouterLink
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                :to="{ name: 'Dashboard_UsersView' }"
+              <button
+                id="mega-menu-full-dropdown-button"
+                data-collapse-toggle="mega-menu-full-dropdown"
+                class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Users
-              </RouterLink>
+                Company
+                <svg
+                  class="w-2.5 h-2.5 ms-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
             </li>
             <li>
-              <RouterLink
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                :to="{ name: 'Dashboard_PostsView' }"
+              <a
+                href="#"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                >Marketplace</a
               >
-                Posts
-              </RouterLink>
             </li>
             <li>
-              <RouterLink
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                :to="{ name: 'Dashboard_ProductsView' }"
+              <a
+                href="#"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                >Resources</a
               >
-                Products
-              </RouterLink>
             </li>
             <li>
-              <RouterLink
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                :to="{ name: 'Dashboard_ProfileView' }"
+              <a
+                href="#"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                >Contact</a
               >
-                Profile
-              </RouterLink>
             </li>
           </ul>
-          </div>
         </div>
-
-        <div class="flex items-end space-x-3 rtl:space-x-reverse">
-
-          <!-- Button Open Dropdown Toggle -->
-          <button
-            @click="toggleDropdown"
-            class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-            id="user-menu-button"
-            aria-expanded="isDropdownVisible"
-          >
-            <span class="sr-only">Open user menu</span>
-            <img
-              class="w-8 h-8 rounded-full"
-              :src="authStore.user.profileImage"
-              alt="User Photo"
-            />
-          </button>
-
-          <!-- Dropdown Menu -->
-          <div
-            v-show="isDropdownVisible"
-            class="absolute right-0 z-50 mt-2 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-          >
-            <div class="px-4 py-3">
-              <span class="block text-sm font-medium text-gray-900 dark:text-white">{{ authStore.user.fullname }}</span>
-              <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ authStore.user.email }}</span>
-            </div>
-            <ul class="py-2">
-              <li>
-                <RouterLink
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200"
-                  :to="{ name: 'Dashboard_ProfileView' }"
+      </div>
+      <div
+        id="mega-menu-full-dropdown"
+        class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
+      >
+        <div
+          class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6"
+        >
+          <ul>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Online Stores</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
                 >
-                  Profile
-                </RouterLink>
-              </li>
-              <li>
-                <RouterLink
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200"
-                  :to="{ name: 'SettingView' }"
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Segmentation</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
                 >
-                  Settings
-                </RouterLink>
-              </li>
-              <li>
-                <button
-                  @click="logout"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200"
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Marketing CRM</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
                 >
-                  Sign out
-                </button>
-              </li>
-            </ul>
-          </div>
-
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Online Stores</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
+                >
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Segmentation</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
+                >
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <div class="font-semibold">Marketing CRM</div>
+                <span class="text-sm text-gray-500 dark:text-gray-400"
+                  >Connect with third-party tools that you're already
+                  using.</span
+                >
+              </a>
+            </li>
+          </ul>
         </div>
-
       </div>
-
-      <!-- Guest Menu -->
-      <div v-else>
-        <ul class="flex space-x-4">
-          <li>
-            <RouterLink
-              class="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
-              :to="{ name: 'LoginView' }"
-            >
-              Login
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink
-              class="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
-              :to="{ name: 'RegisterView' }"
-            >
-              Register
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-
-    </div>
-  </nav>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import ModalLogin from '@/components/ModalLogin.vue'
 </script>
 
 <template>
   <div>
-    
+    <div class="w-full flex items-center">
+      
+    </div>
   </div>
 </template>
